10afb49138741ed121ba0a549edf6877cac22605
Mark Biesheuvel <mail@markbiesheuvel.nl>
2016-12-06 10:41:46 +0100

    Apply standardjs codestyle

--- index.js
+++ index.js
@@ -7,56 +7,57 @@
  * @see http://docs.aws.amazon.com/apigateway/latest/developerguide/use-custom-authorizer.html
  * @author Chris Moyer <cmoyer@aci.info>
  */
-var jwt = require('jsonwebtoken');
-var fs = require('fs');
-var cert = fs.readFileSync('cert.pem');
+var jwt = require('jsonwebtoken')
+var fs = require('fs')
+var cert = fs.readFileSync('cert.pem')
 
-function generatePolicyDocument(data, effect, resource) {
-	// Make data available to the authorizer
-	var authResponse = {
-		context: data,
-		principalId: data.id
-	};
+var generatePolicyDocument = function (data, effect, resource) {
+  // Make data available to the authorizer
+  var authResponse = {
+    context: data,
+    principalId: data.id
+  }
 
-	if (effect && resource) {
-		authResponse.policyDocument = {
-			Version: '2012-10-17', // default version
-			Statement: [
-				{
-					Action: 'execute-api:Invoke', // default action
-					Effect: effect,
-					Resource: resource
-				}
-			]
-		};
-	}
-	return authResponse;
+  if (effect && resource) {
+    authResponse.policyDocument = {
+      Version: '2012-10-17', // default version
+      Statement: [
+        {
+          Action: 'execute-api:Invoke', // default action
+          Effect: effect,
+          Resource: resource
+        }
+      ]
+    }
+  }
+
+  return authResponse
 }
 
 /**
  * Handle requests from API Gateway
  * "event" is an object with an "authorizationToken"
  */
-exports.handler = function jwtHandler(event, context){
-	var token = event.authorizationToken.split(' ');
-	if(token[0] === 'Bearer'){
-		// Token-based re-authorization
-		// Verify
-		jwt.verify(token[1], cert, function(err, data){
-			if(err){
-				console.log('Verification Failure', err);
-				context.fail('Unauthorized');
-			} else if (data && data.id){
-				console.log('LOGIN', data);
-				context.succeed(generatePolicyDocument(data, 'Allow', event.methodArn));
-			} else {
-				console.log('Invalid User', data);
-				context.fail('Unauthorized');
-			}
-		});
-	} else {
-		// Require a "Bearer" token
-		console.log('Wrong token type', token[0]);
-		context.fail('Unauthorized');
-	}
-};
+exports.handler = function (event, context) {
+  var token = event.authorizationToken.split(' ')
+  if (token[0] === 'Bearer') {
+    // Token-based re-authorization
+    // Verify
+    jwt.verify(token[1], cert, function (err, data) {
+      if (err) {
+        console.log('Verification Failure', err)
+        context.fail('Unauthorized')
+      } else if (data && data.id) {
+        console.log('LOGIN', data)
+        context.succeed(generatePolicyDocument(data, 'Allow', event.methodArn))
+      } else {
+        console.log('Invalid User', data)
+        context.fail('Unauthorized')
+      }
+    })
+  } else {
+    // Require a "Bearer" token
+    console.log('Wrong token type', token[0])
+    context.fail('Unauthorized')
+  }
+}
